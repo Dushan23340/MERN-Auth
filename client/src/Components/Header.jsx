392cbde531a76f78b623096f46abd9301f03fcff
Dushan Chamuditha <dasanayakadushan@gmail.com>
2024-08-19 07:53:24 +0530

    Do some changes in signup UI

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -2,25 +2,31 @@ import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <div className="bg-blue-400">
-      <div className="flex justify-between items-center max-w-6xl mx-auto">
-        <Link to="/" className="flex items-center">
-          {/* Logo Image */}
-          <img src="src/assets/images/logo.png" alt="Logo" className="h-12 w-12 mr-2" />
-          {/* Title */}
-        </Link>
+    <div className="bg-blue-300">
+      <div className="flex justify-between items-center max-w-6xl mx-36">
+      <Link to="/" className="flex items-center space-x-2">
+  {/* Logo Image */}
+  <img src="src/assets/images/logo.png" alt="Logo" className="h-16 w-16" />
+  {/* Title */}
+  <span className="text-xl font-bold font-serif bg-gradient-to-r from-blue-500 to-blue-800 bg-clip-text text-transparent">
+  BLUEWAVE
+</span>
+
+</Link>
+
          {/* Search Bar */}
-         <form className="flex items-center border border-white rounded-full overflow-hidden mr-96">
-            <input
-              type="text"
-              placeholder="Search..."
-              className="px-4 py-2 border-none outline-none"
-            />
-            <button type="submit" className="bg-blue-500 text-white px-4 py-2 hover:bg-blue-700 transition">
-              Search
-            </button>
-          </form>
-        <ul className="flex items-center gap-4 font-medium">
+       <form className="flex items-center border border-white rounded-full overflow-hidden mr-64">
+          <input
+             type="text"
+             placeholder="Search..."
+             className="px-2 py-1 border-none outline-none"
+          />
+          <button type="submit" className="bg-blue-500 text-white px-2 py-1 hover:bg-blue-700 transition">
+          Search
+          </button>
+       </form>
+
+        <ul className="flex items-center gap-7 font-medium font-serif">
           <Link to="/">
             <li>Home</li>
           </Link>
